4143ae8d98167357faa965d996f0ceb5a6ab25ef
Matteo von Haxthausen <matteovhaxt@gmail.com>
2023-03-08 23:03:09 +0100

    bloc state improvements

--- lib/presentation/blocs/bloc.dart
+++ lib/presentation/blocs/bloc.dart
@@ -7,25 +7,71 @@ import 'package:http/http.dart' as http;
 abstract class GuessEvent {}
 
 class AgeGuess extends GuessEvent {
-  AgeGuess({required this.name});
+  AgeGuess({
+    required this.name,
+  });
 
   final String name;
 }
 
-class GuessBloc extends Bloc<GuessEvent, Guess?> {
-  GuessBloc() : super(null) {
+class GuessState {
+  GuessState({
+    required this.result,
+    required this.loading,
+    required this.error,
+  });
+  Guess? result;
+  bool loading = false;
+  dynamic error;
+
+  GuessState copyWith({
+    Guess? result,
+    bool? loading,
+    dynamic error,
+  }) =>
+      GuessState(
+        result: result ?? this.result,
+        loading: loading ?? this.loading,
+        error: error ?? this.error,
+      );
+}
+
+class GuessBloc extends Bloc<GuessEvent, GuessState> {
+  GuessBloc()
+      : super(
+          GuessState(
+            result: null,
+            loading: false,
+            error: null,
+          ),
+        ) {
     final repository = HttpApiRepository(
       api: Api(),
       client: http.Client(),
     );
     on<AgeGuess>((event, emit) async {
       try {
+        emit(
+          state.copyWith(
+            loading: true,
+          ),
+        );
         final result = await repository.getGuess(
           event.name,
         );
-        emit(result);
+        emit(
+          state.copyWith(
+            result: result,
+            loading: false,
+          ),
+        );
       } catch (error) {
-        throw Exception(error);
+        emit(
+          state.copyWith(
+            loading: false,
+            error: error,
+          ),
+        );
       }
     });
   }
